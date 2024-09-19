a2cc4a36f0e202ac08031a287c084ed1915160d0
orun-sunny <sunny.mukut@gmail.com>
2024-09-19 19:18:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+      <h1>Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
